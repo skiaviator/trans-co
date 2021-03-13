8c485dcf6be56e78482b97f0be3fc7cdf40d44e8
DamianZamojski <damian.zam1f20@gmail.com>
2021-03-13 15:27:06 +0100

    Api initialized

---
+++ api/src/test/java/transport/co/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package transport.co.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
